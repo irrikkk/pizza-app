7490c262f436bdb98e5efe9f5805ce3b899be6d9
Ирина Новикова <irishnovikova@icloud.com>
2026-01-26 23:18:37 +0700

    Initial Commit

---
+++ PizzaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaApp
+//
+//  Created by Ирина Новикова on 26.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
